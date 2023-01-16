8eab206914a5464dbe86a755bf2d7fb69344d96e
Antonin <9219052+antonincms@users.noreply.github.com>
2023-01-16 14:53:37 +0100

    Improvements and fix to rollout captures script
    * Remove useless check from capture rollout script
      Checking secret is not needed if the image is ok and the pod is scheduled
    * Remove useless uncordonning
      This is not needed since new overprovisioner nodes should not be cordonned
    * Avoid depending on awk and jq when kubectl can do it with
      field-selector or jsonpath
    * Use kubectl wait instead of waiting by checking manually pod nodeName
    * Fix timestamps sometimes incorrectly computed
    * Use bash strict mode : http://redsymbol.net/articles/unofficial-bash-strict-mode/
    * Bound some variable to their functions and normalized check_* functions
    * Renamed some strangely named variable
    * Improved logs messages
    * Normalize kubectl usage
    * Normalize variable syntax
    * Fix shellcheck warning about globbing and word splitting : shellcheck.net/wiki/SC2086

--- rollout_captures_asg.sh
+++ rollout_captures_asg.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -eu pipeline
+set -euo pipefail
 
 OVERPROVISIONER_NAMESPACE=cluster-overprovisioner
 OVERPROVISIONER_DEPLOYMENT=cluster-overprovisioner-captures-overprovisioner
 CAPTURE_NAMESPACE=reference
 CAPTURE_IMAGE=$(
-  kubectl get deployment -n reference -o json |
-    jq -r '.items[] | select(.metadata.name | endswith("capture")) | .spec.template.spec.containers[0].image' |
-    head -1
+  kubectl -n "${CAPTURE_NAMESPACE}" get deployments \
+    --selector app.kubernetes.io/name=capture \
+    -o jsonpath='{.items[0].spec.template.spec.containers[0].image}'
 )
 
 # Colors
@@ -59,103 +59,73 @@ function redb {
 }
 
 function fail_and_exit {
-  redb "$1"
+  redb "${1}"
   exit 1
 }
 
 function drain {
-  yellow "Draining $1"
-  kubectl drain $1 --ignore-daemonsets=true --delete-emptydir-data=true --timeout=120s --skip-wait-for-delete-timeout=1
-  green "$1 successfully drained."
+  yellow "Draining ${1}"
+  kubectl drain "${1}" --ignore-daemonsets=true --delete-emptydir-data=true --timeout=120s --skip-wait-for-delete-timeout=1
+  green "${1} successfully drained."
 }
 
 function releaseip {
+  local pending_pods_count
   externalip_name=$(
-    kubectl get externalips -o json |
-      jq -r --arg n $1 '.items[] | select(.spec.nodeName == $n) | .metadata.name'
+    kubectl get externalips \
+      -o jsonpath="{.items[?(@.spec.nodeName==\"${1}\")].metadata.name}"
   )
 
-  for ip in $externalip_name; do
-    green "Disassociate EIP $ip from $1"
-    kubectl patch externalips $ip --type=merge -p '{"spec": {"nodeName": ""}}'
+  for ip in ${externalip_name}; do
+    green "Disassociate EIP ${ip} from ${1}"
+    kubectl patch externalips "${ip}" --type=merge -p '{"spec": {"nodeName": ""}}'
   done
 }
 
 function kubestatic_unlabel {
-  green "Remove kubestatic label on node $1"
-  kubectl label node $1 kubestatic.quortex.io/externalip-auto-assign-
+  green "Remove kubestatic label on node ${1}"
+  kubectl label node "${1}" kubestatic.quortex.io/externalip-auto-assign-
 }
 
 function check_capture_status {
-  echo -n "No capture pod should be pending, checking... "
-  for status in $(kubectl -n $CAPTURE_NAMESPACE get pods \
-    -l app.kubernetes.io/name=capture -o json |
-    jq -r .items[].status.phase); do
-    if [[ ${status} != "Running" ]]; then
-      fail_and_exit "All capture pods are not in a Running state, I can't continue."
-    fi
-  done
+  echo -n "Checking that all capture pods are running... "
+  local pending_pods_count
+  pending_pods_count=$(
+    kubectl -n "${CAPTURE_NAMESPACE}" get pods \
+      --selector app.kubernetes.io/name=capture \
+      --field-selector status.phase!=Running -o name | wc -w
+  )
+  if [ "${pending_pods_count}" -ne 0 ]; then
+    fail_and_exit "All capture pods are not in a Running state, I can't continue."
+  fi
   echo -e "${green}Ok${end}"
 }
 
 function check_overprovisioner_status {
-  echo -n "No capture overprovisioner should be pending, checking... "
-  for status in $(kubectl get -n $OVERPROVISIONER_NAMESPACE pods \
-    -l app.cluster-overprovisioner/deployment=captures-overprovisioner -o json |
-    jq -r .items[].status.phase); do
-    if [[ ${status} == "Pending" ]]; then
-      fail_and_exit "Some overprovisioner pods are in a Pending state, I can't continue."
-    fi
-  done
+  echo -n "Checking that all captures-overprovisioner pods are running... "
+  local pending_pods_count
+  pending_pods_count=$(
+    kubectl -n "${OVERPROVISIONER_NAMESPACE}" get pods \
+      --selector app.cluster-overprovisioner/deployment=captures-overprovisioner \
+      --field-selector status.phase!=Running -o name | wc -w
+  )
+  if [ "${pending_pods_count}" -ne 0 ]; then
+    fail_and_exit "Some overprovisioner pods are in a Pending state, I can't continue."
+  fi
   echo -e "${green}Ok${end}"
 }
 
-function disable_unschedulable_ipassign {
-  green "Make sure unschedulable node can not receive IP"
-  local unschedulable_nodes=$(
-    kubectl get nodes -l group=captures-fix-group -o json |
-      jq -r '.items[] | select(.spec.unschedulable = true) | .metadata.name'
-  )
-
-  for node in ${unschedulable_nodes}; do
-    kubestatic_unlabel $node
-  done
-}
-
-function cordon_all {
-  green "First, cordon all schedulable capture nodes.."
-  nodes=$(
-    kubectl get nodes -l group=captures-fix-group -o json |
-      jq -r '.items[] | select(.spec.unschedulable != true) | .metadata.name'
-  )
-
-  for node in ${nodes}; do
-    kubectl cordon ${node}
-  done
-}
-
-function check_OP_image {
+function check_overprovisioner_image {
   echo -n "Capture overprovisioner image should match capture pod, checking... "
-  OP_IMAGE=$(
-    kubectl get deployment -n ${OVERPROVISIONER_NAMESPACE} ${OVERPROVISIONER_DEPLOYMENT} -o json |
-      jq -r .spec.template.spec.containers[0].image
+  local overprovisioner_image
+  overprovisioner_image=$(
+    kubectl -n "${OVERPROVISIONER_NAMESPACE}" get deployment "${OVERPROVISIONER_DEPLOYMENT}" \
+      -o jsonpath='{.spec.template.spec.containers[0].image}'
   )
 
-  [[ ${OP_IMAGE} == ${CAPTURE_IMAGE} ]] || fail_and_exit "You must set capture overprovisioner image as the capture pod."
-  echo -e "${green}Ok${end}"
-}
-
-function check_OP_GCR_secret {
-  echo -n "Capture overprovisioner should have a secret and reference it to access GCR capture image, checking... "
-  kubectl get secret -n ${OVERPROVISIONER_NAMESPACE} quortex-gcr > /dev/null 2>&1 || fail_and_exit "quortex-gcr secret is missing in namespace ${OVERPROVISIONER_NAMESPACE}"
-
-  # check OP has a imagePullSecret block that points to existing secret
-  OP_DEPLOY_SECRET_NAME=$(
-    kubectl get deployment -n ${OVERPROVISIONER_NAMESPACE} ${OVERPROVISIONER_DEPLOYMENT} -o json |
-      jq -r .spec.template.spec.imagePullSecrets[0].name 2>/dev/null
-  )
-  [[ ${OP_DEPLOY_SECRET_NAME} != "quortex-gcr" ]] && fail_and_exit "You must set the GCR image pull secret in capture overprovisioner deployment."
-
+  if [ "${overprovisioner_image}" != "${CAPTURE_IMAGE}" ]; then
+    fail_and_exit "You must set capture overprovisioner image as the capture pod."
+  fi
   echo -e "${green}Ok${end}"
 }
 
@@ -166,8 +136,7 @@ function now {
 [[ ! $(which kubectl) ]] && fail_and_exit "kubectl CLI not found"
 
 check_capture_status
-check_OP_image
-check_OP_GCR_secret
+check_overprovisioner_image
 check_overprovisioner_status
 white "Note that unschedulable capture nodes will be ignored."
 echo
@@ -177,141 +146,114 @@ whiteb "Capture overprovisioner namespace  : ${OVERPROVISIONER_NAMESPACE}"
 whiteb "Capture image                      : ${CAPTURE_IMAGE}"
 whiteb "Kube context                       : $(kubectl config current-context)"
 echo -n "Continue? y/n "
-read answer
+read -r answer
 
-if [[ $answer != "y" ]]; then
+if [[ "${answer}" != "y" ]]; then
   fail_and_exit "Did not receive [y], exiting."
 fi
 
-disable_unschedulable_ipassign
-cordon_all
-#update_OP_image
+# List the nodes that are currently schedulable
+nodes_to_rollout=$(
+  kubectl get nodes \
+    --selector group=captures-fix-group \
+    --field-selector spec.unschedulable=false \
+    -o jsonpath="{.items[*]['metadata.name']}"
+)
+whiteb "The following nodes will be processed : ${nodes_to_rollout}"
+
+green "Cordoning ${nodes_to_rollout} ..."
+for node in ${nodes_to_rollout}; do
+  kubectl cordon "${node}"
+done
+
+green "Remove kubestatic label on ${nodes_to_rollout} ..."
+for node in ${nodes_to_rollout}; do
+  kubectl label node "${node}" kubestatic.quortex.io/externalip-auto-assign-
+done
 
 # First rollout overprovisioner nodes
-OP_nodesName=$(
-  kubectl get pod -n $OVERPROVISIONER_NAMESPACE \
-    -l app.cluster-overprovisioner/deployment=captures-overprovisioner -o json |
-    jq -r .items[].spec.nodeName | uniq
+overprovisioner_nodes=$(
+  kubectl -n "${OVERPROVISIONER_NAMESPACE}" get pod \
+    --selector app.cluster-overprovisioner/deployment=captures-overprovisioner \
+    --sort-by=.spec.nodeName -o jsonpath="{.items[*]['spec.nodeName']}" | uniq
 )
 green "Draining all nodes with overprovisioner"
-for node in $OP_nodesName; do
+for node in ${overprovisioner_nodes}; do
   yellow "Draining ${node}"
-  kubectl drain ${node} --ignore-daemonsets=true --delete-emptydir-data=true --force --timeout=120s
+  kubectl drain "${node}" --ignore-daemonsets --delete-emptydir-data --force --timeout=120s
 done
 
-OP_podsName=$(
-  kubectl get pod -n $OVERPROVISIONER_NAMESPACE \
-    -l app.cluster-overprovisioner/deployment=captures-overprovisioner \
-    -o name
-)
+green "Wait for capture overprovisioners to be rescheduled and ready"
+kubectl -n "${OVERPROVISIONER_NAMESPACE}" wait pod --for=condition=ready --timeout=10m \
+  --selector "app.cluster-overprovisioner/deployment=captures-overprovisioner"
 
-green "Wait for capture overprovisioners to reschedule..."
-TIMEOUT=300
-for pod in $OP_podsName; do
-  nodeName=null
-  while [[ $nodeName == "null" ]]; do
-    echo "nodename=$nodeName, waiting."
-    # If nodeName is non-null then we consider it to be acceptable
-    nodeName=$(
-      kubectl get -n ${OVERPROVISIONER_NAMESPACE} ${pod} -o json |
-        jq -r .spec.nodeName
-    )
-    sleep 1
-
-    TIMEOUT=$((TIMEOUT - 1))
-    if [[ $TIMEOUT -lt 0 ]]; then
-      fail_and_exit capture overprovisioner never scheduled.
-    fi
-  done
-  white "$pod is scheduled."
-done
-
-for node in ${nodes}; do
+for node in ${nodes_to_rollout}; do
   # Get some info about the cluster state
   capture_pods=$(
-    kubectl get pods -A --field-selector spec.nodeName=${node} |
-      grep capture | awk '{print $2}'
+    kubectl get pods -A \
+      --selector app.kubernetes.io/name=capture \
+      --field-selector "spec.nodeName=${node}" \
+      -o jsonpath="{.items[*]['metadata.name']}"
   )
+
   # probably only overprovisioner pods on this node, skipping
-  [[ -z $capture_pods ]] && continue
+  if [[ -z "${capture_pods}" ]]; then
+    whiteb "Node ${node} does not have any captures, skipping"
+    continue
+  fi
 
-  white "Working on ${node}"
+  whiteb "Migrating the node ${node}"
 
-  # Make an OP node (any of them) available for capture pods
-  OP_nodeName=$(
-    kubectl get pod -n cluster-overprovisioner \
-      -l app.cluster-overprovisioner/deployment=captures-overprovisioner \
-      -o json |
-      jq -r .items[0].spec.nodeName
+  # Make an overprovisioner node (any of them) available for capture pods
+  overprovisioner_node=$(
+    kubectl -n "${OVERPROVISIONER_NAMESPACE}" get pod \
+      --selector app.cluster-overprovisioner/deployment=captures-overprovisioner \
+      -o jsonpath="{.items[0]['spec.nodeName']}"
   )
 
   # Start capture migration
+  stamp=$(now)
+  kubestatic_unlabel "${node}"
+  yellow "==> Unlabeling capture node took $(($(now) - stamp))s."
+
   start_downtime=$(now)
-  kubestatic_unlabel $node
-  yellow "==> Unlabeling capture node took $(($(now) - start_downtime))s."
-  stamp=$(now)
+  releaseip "${node}"
+  yellow "==> Release EIP from capture node took $(($(now) - start_downtime))s."
 
-  green "Free $OP_nodeName overprovisioner node"
-  kubectl uncordon $OP_nodeName
-  yellow "==> Making overprovisioner node schedulable took $(($(now) - stamp))s."
   stamp=$(now)
-
-  releaseip $node
-  yellow "==> Release EIP from capture node took $(($(now) - stamp))s."
-  stamp=$(now)
-
   for pod in ${capture_pods}; do
-    kubectl delete pod --force=true -n ${CAPTURE_NAMESPACE} ${pod}
-    yellow "==> Force deleted pod ${pod} in $(($(now) - stamp))s."
-    stamp=$(now)
+    kubectl -n "${CAPTURE_NAMESPACE}" delete pod --force=true "${pod}"
   done
+  yellow "==> Force deleted ${capture_pods} in $(($(now) - stamp))s."
 
-  releaseip $OP_nodeName
+  stamp=$(now)
+  releaseip "${overprovisioner_node}"
   yellow "==> Release EIP from overprovisioner node took $(($(now) - stamp))s."
-  stamp=$(now)
 
-  drain $node
-  yellow "==> Draining took $(($(now) - start_downtime))s."
   stamp=$(now)
+  drain "${node}"
+  yellow "==> Draining took $(($(now) - stamp))s."
 
   # wait for capture pods to be scheduled on new node
+  stamp=$(now)
   pending_capture_pods=$(
-    kubectl get pods -n $CAPTURE_NAMESPACE -o json |
-      jq -r '.items[] | select(.status.phase == "Pending") | .metadata.name'
+    kubectl -n "${CAPTURE_NAMESPACE}" get pods \
+      --selector app.kubernetes.io/name=capture \
+      --field-selector status.phase!=Running \
+      -o jsonpath="{.items[*]['metadata.name']}"
   )
-  for pod in $pending_capture_pods; do
-    green "Waiting for $pod to be scheduled..."
-    kubectl wait -n $CAPTURE_NAMESPACE --for=condition=ready pod ${pod} --timeout=300s
+  for pod in ${pending_capture_pods}; do
+    green "Waiting for ${pod} to be scheduled..."
+    kubectl -n "${CAPTURE_NAMESPACE}" wait pod "${pod}" \
+      --for=condition=ready --timeout=5m
   done
-  yellow "==> Waiting for capture pod to be ready took $(($(now) - stamp))s."
-  end_downtime=$(date +%s)
+  end_downtime=$(now)
+
+  yellow "==> Waiting for capture pod to be ready took $((end_downtime - stamp))s."
   yellowb "==> capture pods on ${node} were unavailable for $((end_downtime - start_downtime)) seconds."
 
-  # wait for OP pods to be scheduled
-  OP_podsName=$(
-    kubectl get pod -n ${OVERPROVISIONER_NAMESPACE} \
-      -l app.cluster-overprovisioner/deployment=captures-overprovisioner \
-      -o name
-  )
-  green "Wait for overprovisioners to be rescheduled too"
-  for pod in ${OP_podsName}; do
-    green "Waiting for ${pod} to be scheduled..."
-    nodeName=null
-    while [[ $nodeName == "null" ]]; do
-      # If nodeName is non-null then we consider it to be acceptable
-      nodeName=$(
-        kubectl get -n ${OVERPROVISIONER_NAMESPACE} ${pod} -o json |
-          jq -r .spec.nodeName
-      )
-      sleep 1
-
-      TIMEOUT=$((TIMEOUT - 1))
-
-      if [[ $TIMEOUT -lt 0 ]]; then
-        fail_and_exit capture overprovisioner never scheduled.
-      fi
-
-    done
-    white "$pod is scheduled."
-  done
+  green "Wait for overprovisioners to be rescheduled and ready"
+  kubectl -n "${OVERPROVISIONER_NAMESPACE}" wait pod \
+    --for=condition=ready --timeout=10m \
+    --selector "app.cluster-overprovisioner/deployment=captures-overprovisioner"
 done
