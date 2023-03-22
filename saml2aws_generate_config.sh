43f9f00f1189cb9d318199d2db1ab0ec513ba885
Vincent Marguerie <24724195+vincentmrg@users.noreply.github.com>
2023-03-22 15:28:46 +0100

    improve and format code

--- saml2aws_generate_config.sh
+++ saml2aws_generate_config.sh
@@ -17,28 +17,25 @@ num_lines=$(echo "$roles_list" | wc -l)
 current_line=0
 
 # Iterate over role list to generate aws config
-echo "$roles_list" | while read line; do
-  current_line=$(($current_line + 1))
+echo "$roles_list" | while read -r line; do
+  current_line=$((${current_line} + 1))
 
   # Capture AWS account alias
-  if [[ $line =~ $ACCOUNT_REGEX ]]
-  then
+  if [[ $line =~ $ACCOUNT_REGEX ]]; then
     account="${BASH_REMATCH[1]}"
     echo "## ${line}"
     echo "##"
   fi
 
   # Capture role name
-  if [[ $line =~ $ROLE_REGEX ]]
-  then
+  if [[ $line =~ $ROLE_REGEX ]]; then
     role_arn=$line
     role_name="${BASH_REMATCH[1]}"
     profile="${account}/${role_name}"
     echo "[profile ${profile}]"
     echo "output = json"
     echo "credential_process = saml2aws login --skip-prompt --quiet --credential-process --role ${role_arn} --profile ${profile}-saml2aws"
-    if [[ $current_line -ne $num_lines ]]
-    then 
+    if [[ $current_line -ne $num_lines ]]; then
       echo ""
     fi
   fi
