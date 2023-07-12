ce0682e39f0b48836e48ccf16fea2e5cb1f42409
Antonin <9219052+antonincms@users.noreply.github.com>
2023-07-12 17:51:29 +0200

    Fix bugs and improved script output

--- init_tf_backend_azure.sh
+++ init_tf_backend_azure.sh
@@ -39,18 +39,23 @@ while getopts "h:r:a:c:s" opt; do
         STORAGE_ACCOUNT_NAME=$OPTARG
         ;;
     c)
-        CONTAINER_NAME=$OPTARG
+        STORAGE_CONTAINER_NAME=$OPTARG
         ;;
     *)
-        echo "Unsupported flag provided : ${OPTARG}".
+        echo "Unsupported flag provided : ${opt}".
         help
         exit 1
         ;;
     esac
 done
 
-if [ "$NAME" == "" ]; then
-    echo "Name was not specified, aborting !"
+if [ "$STORAGE_ACCOUNT_NAME" == "" ]; then
+    echo "Storage account name was not specified, aborting !"
+    exit 1
+fi
+
+if [ "$STORAGE_CONTAINER_NAME" == "" ]; then
+    echo "Storage container name was not specified, aborting !"
     exit 1
 fi
 
@@ -62,11 +67,13 @@ az storage account create \
     --name "${STORAGE_ACCOUNT_NAME}" \
     --resource-group "${RESOURCE_GROUP_NAME}" \
     --sku Standard_LRS \
-    --encryption-services blob
+    --encryption-services blob \
+    --output none
 
 # Create Storage Account Container
-echo "Creating Storage Account : ${CONTAINER_NAME}"
+echo "Creating Storage Account : ${STORAGE_CONTAINER_NAME}"
 az storage container create \
-    --name "${CONTAINER_NAME}" \
+    --name "${STORAGE_CONTAINER_NAME}" \
     --account-name "${STORAGE_ACCOUNT_NAME}" \
-    --auth-mode login
+    --auth-mode login \
+    --output none
